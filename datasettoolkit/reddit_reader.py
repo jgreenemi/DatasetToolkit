50794418a04998472ce7dbdeea9071be0dbf3eb3
Joseph Greene <jgreenemi@gmail.com>
2017-08-25 01:17:07 -0700

    Start a simple RedditReader.

---
+++ datasettoolkit/reddit_reader.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# Reads reddit self-text posts and writes them to a file!
+
+import requests
+from pprint import pprint
+
+
+class RedditReader():
+    def __init__(self):
+        """
+        A class for handling both the retrieval of text data from the reddit website, and for passing that to files
+        per interactive user instruction. Use querystrings for limiting response, i.e. /r/bitcoin/new/.json?limit=1
+
+        """
+
+        self.subreddit_url = r'http://www.reddit.com/r/bitcoin/new/.json'
+        self.headers = {
+            'User-Agent': r'DSTK-RedditReader/0.1'
+        }
+        self.classes = {
+            '1': 'datasets/Exp01-reddit-bitcoin-faqs.txt',
+            '2': 'datasets/Exp01-reddit-bitcoin-nonfaqs.txt'
+        }
+
+        return
+
+    def read(self):
+        """
+        Ask reddit for some data from a sub, and we'll step through each returned item for processing.
+
+        :return:
+        """
+
+        try:
+            reddit_response = requests.get(self.subreddit_url, headers=self.headers).json()['data']
+            reddit_post_list = reddit_response['children']
+
+            for reddit_post in reddit_post_list:
+                if reddit_post['data']['is_self']:
+                    # This is a self-post. Save the text, ommitting newlines.
+                    candidate_text = reddit_post['data']['selftext']
+                    candidate_text = candidate_text.replace('\n', ' ').replace('\r', '')
+                    
+                    # Can prompt the user now about which class this should be sent to.
+                    self.prompt_and_write(candidate_text)
+            return
+
+        except Exception as e:
+            self.exception_handling(e)
+
+    def prompt_and_write(self, candidate_text):
+        """
+        When items are passed in, you can ask the viewer to classify the text for writing to one file, or another, based
+        on what the text is about. i.e. a selftext post from reddit is shown to the user, and they're asked if it should
+        be categorized as type A or type B. Based on the response, the text is appended to a file for type A or B text.
+        :return:
+        """
+        try:
+            print()
+        except Exception as e:
+            self.exception_handling(e)
+
+        return
+
+    @staticmethod
+    def exception_handling(e):
+        print('Exception: {}'.format(e))
+        return
+
+def main():
+    """
+    Generally this is what invokes the actual reading from reddit.
+    :return:
+    """
+    reddit_client = RedditReader()
+    reddit_client.read()
+    return
+
+if __name__ == '__main__':
+    main()
